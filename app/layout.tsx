22ed0316cd8c70474faabc6f2fcf7a168a2e0c45
daidbou <89360632+daidbou@users.noreply.github.com>
2024-05-20 21:35:44 +0900

    création de la page d'accueil

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import '@styles/globals.css';
-import Nav from "@components/Nav"
+
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
     title: 'Promptopia',
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const Home = () => {
         <h1 className="head_text text-center">
             Discover & Share
             <br className="max-md:hidden" />
-            <span className="orange_gradient text-center">AI-Powered Prompts</span>
+            <span className="orange_gradient text-center"> AI-Powered Prompts </span>
             <p className="desc text-center">Hello, my name is David, and this is my attempt to create an AI prompting tool for modern world to discover, create and share creative prompts.</p>
         </h1>
         <Feed />
